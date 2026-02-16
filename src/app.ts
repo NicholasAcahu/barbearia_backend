68a0755882ae4ea4344d93fec8655f7cd70d5f1b
Eduardo Nascimento <eduardonascimento60650@gmail.com>
2026-02-15 21:01:37 -0300

    configurando em Ts

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+console.log("Servidor da Barbearia iniciado com sucesso!");
+
+// Exemplo simples de lógica
+const saudacao = (nome: string) => {
+  return `Bem-vindo à Barbearia, ${nome}!`;
+};
+
+console.log(saudacao("Cliente"));
